cf43c11f0beffe9d731b38388a8d07268dfd8225
MiketheBite <migueljcpinto@mail.com>
2023-12-09 23:53:49 +0100

    google authentication

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,8 +5,37 @@ import React from "react";
 import { FaBug } from "react-icons/fa6";
 import { usePathname } from "next/navigation";
 import classnames from "classnames";
+import { useSession } from "next-auth/react";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
+import { Skeleton } from "@/app/components";
 
 const NavBar = () => {
+  return (
+    <nav className="border-b mb-5 px-5 py-3">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            {" "}
+            <Link href="/">
+              <FaBug />
+            </Link>
+            <NavLinks />
+          </Flex>
+          <AuthStatus />
+        </Flex>
+      </Container>
+    </nav>
+  );
+};
+
+const NavLinks = () => {
   const currentPath = usePathname();
 
   const links = [
@@ -15,26 +44,58 @@ const NavBar = () => {
   ];
 
   return (
-    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      <Link href="/">
-        <FaBug />
-      </Link>
-      <ul className="flex space-x-6 ">
-        {links.map((link) => (
+    <ul className="flex space-x-6 ">
+      {links.map((link) => (
+        <li key={link.href}>
           <Link
-            key={link.href}
             className={classnames({
-              "text-zinc-900": link.href === currentPath,
-              "text-zinc-500": link.href !== currentPath,
-              "hover:text-zinc-800 transition-colors": true,
+              "nav-link": true,
+              "!text-zinc-900": link.href === currentPath,
             })}
             href={link.href}
           >
             {link.label}
           </Link>
-        ))}
-      </ul>
-    </nav>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+
+  if (status === "loading") return <Skeleton width="3rem" />;
+
+  if (status === "unauthenticated")
+    return (
+      <Link className="nav-link" href="/api/auth/signin">
+        Login
+      </Link>
+    );
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session!.user!.image!}
+            fallback="?"
+            size="2"
+            radius="full"
+            className="cursor-pointer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size="2">{session!.user!.email}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href="/api/auth/signout">Log out</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
   );
 };
 
