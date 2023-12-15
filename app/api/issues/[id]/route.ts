28de7d67e5869c2edea6f56bbeb48a9e66aaa070
MiketheBite <migueljcpinto@mail.com>
2023-12-16 00:09:22 +0100

    assign issue to a user

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import authOptions from "@/app/auth/authOptions";
-import { pathIssueSchema } from "@/app/validationSchemas";
+import { patchIssueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
@@ -12,17 +12,20 @@ export async function PATCH(
   if (!session) return NextResponse.json({}, { status: 401 });
 
   const body = await request.json();
-  const validation = pathIssueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
   if (!validation.success)
-    return NextResponse.json(validation.error.format(), { status: 400 });
+    return NextResponse.json(validation.error.format(), {
+      status: 400,
+    });
 
   const { assignedToUserId, title, description } = body;
+
   if (assignedToUserId) {
     const user = await prisma.user.findUnique({
       where: { id: assignedToUserId },
     });
     if (!user)
-      return NextResponse.json({ error: "Invalid user." }, { status: 404 });
+      return NextResponse.json({ error: "Invalid user." }, { status: 400 });
   }
 
   const issue = await prisma.issue.findUnique({
@@ -42,24 +45,21 @@ export async function PATCH(
 
   return NextResponse.json(updatedIssue);
 }
-
 export async function DELETE(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
   const session = await getServerSession(authOptions);
   if (!session) return NextResponse.json({}, { status: 401 });
-
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
 
   if (!issue)
-    return NextResponse.json({ error: "Invalid Issue." }, { status: 404 });
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
 
   await prisma.issue.delete({
     where: { id: issue.id },
   });
-
   return NextResponse.json({});
 }
