3f702c04ddf16742ab3dbfdd1455a8c231b8c100
MiketheBite <migueljcpinto@mail.com>
2024-01-02 21:18:18 +0100

    pagination implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return <div>Hello YOUU!!!</div>;
 }
